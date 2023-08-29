345ea205a7edb4938018e8cc382ab24997795fde
Matias <mrodriguezbegueri@gmail.com>
2023-08-29 20:30:26 -0300

    fin sección 5

--- 02-pokemon-static/app/page.tsx
+++ 02-pokemon-static/app/page.tsx
@@ -23,8 +23,6 @@ const Page: NextPage = async () => {
   const pokemons = await getPokemons();
 
   return (
-    <>
-    <Image alt="" src='/img/banner.png' width={100} height={100} />
       <div className="gap-2 grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-6">
         {pokemons.map((pokemon, index) => (
           <>
@@ -32,7 +30,6 @@ const Page: NextPage = async () => {
           </>
         ))}
       </div>
-    </>
   );
 };
 
